5836bb65c16259a2b3b54cd64a082eb9220057b4
Brian W <39372876+BRWelk@users.noreply.github.com>
2018-05-28 10:25:11 -0700

    flipping cards
    Writing JavaScript to flip cards

--- memoryGame/mgDev/mG.js
+++ memoryGame/mgDev/mG.js
@@ -1,1 +1,13 @@
  // Memory Game Udacity Project
+ // Shuffle board
+
+	 function flip(card) {
+   var pic;
+ 	if (card == 0) {
+ 		pic = "../images/earth.jpg";
+ 	 } else {
+ 	 	pic = "../images/solarSystem.jpg";
+ 	 }
+ 	 document.getElementById('myImage').src = pic;
+ 	}
+ 
