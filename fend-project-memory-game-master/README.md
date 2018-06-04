10945fd4ef8e8c0d3c9af5cb65b23bdb52dbaab0
Brian W <39372876+BRWelk@users.noreply.github.com>
2018-06-03 23:52:39 -0500

    90% finished
    Needs timer

--- fend-project-memory-game-master/README.md
+++ fend-project-memory-game-master/README.md
@@ -19,17 +19,4 @@ This repository is the starter code for _all_ Udacity students. Therefore, we mo
 
 For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
 
-
-
-***A BIG THANKS TO YAHYA ELHARNOY for providing the study jam video. Great descriptions to help me learn DOM manipulation with JavaScript. Thank You Yahya!!!***
-
-Udacity Front End Nano Degree Memory Game Project
-- Creating a memory game to work with HTML, CSS and JavaScript
-- Work to lay down basic foundation with TODO's completed by turn in date.
-  -TODO's
-    - Use images of universe as card images
-    - Offer hints of last 3 moves (add border to cards) if player asks. 
-      - Player gets 1 hint per game
-      - Using hint adds 30 seconds to time
-    - Card images disappear when matched, replaced by a black hole
-    - Record user names and data, to provide win - loss, time and rating information.
+***BIG THANKS TO Yahya Elharony for providing the Study Jam and helping me understand how the JavaScript works for this project. Working with DOM is a challenge, but it is a bit easier now. Thanks again Yahya.***
--- fend-project-memory-game-master/js/app.js
+++ fend-project-memory-game-master/js/app.js
@@ -1,8 +1,26 @@
 /*
  * Create a list that holds all of your cards
  */
+ // Shuffle function from http://stackoverflow.com/a/2450976
+
+
  // grabbing icons for cards from HTML class "card"
 let cards = ['fa fa-diamond', 'fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-bolt', 'fa fa-cube', 'fa fa-cube',  'fa fa-leaf', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-bicycle', 'fa fa-bomb', 'fa fa-bomb'];
+
+function shuffle(cards) {
+    let i = cards.length, j = 0, card;
+
+    while (i--) {
+
+        j = Math.floor(Math.random() * (i+1));
+        
+        card = cards[i];
+        cards[i] = cards[j];
+        cards[j] = card;
+    }
+
+    return card;
+};
 
 const cardTable = document.querySelector(".deck");
 let flippedCards = [];
@@ -16,29 +34,24 @@ function startGame(){
     cardTable.appendChild(card);
     //Click Event for each card
     click(card);
+  }
 }
-}
-
     // click to flip card
 function click(card) {
-card.addEventListener("click", function() {
- 
+card.addEventListener("click", function() { 
     const secondCard = this;
     const firstCard = flippedCards[0];
-
      //existing flipped cards
     if(flippedCards.length === 1) {
-
-      card.classList.add("open", "show");          //adding class to HTML that works with .open .show css
+      card.classList.add("open", "show", "disable");        //adding class to HTML that works with .open .show css
       flippedCards.push(this);                     //this refers to card.addEventListener("click", Function())
 
      //looking for match
      compare(secondCard, firstCard);
 
-    
     } else {
     //else no opened cards
-      card.classList.add("open", "show");//adding class to HTML that works with .open .show css
+      card.classList.add("open", "show", "disable");//adding class to HTML that works with .open .show css
       flippedCards.push(this);//this refers to card.addEventListener("click", Function())
     }
 
@@ -57,46 +70,66 @@ function compare(secondCard, firstCard) {
         } else {
             //setting timer for second card to show
         setTimeout(function() {
-          secondCard.classList.remove("open", "show");
-          firstCard.classList.remove("open", "show");
+          secondCard.classList.remove("open", "show", "disable");
+          firstCard.classList.remove("open", "show", "disable");
           flippedCards = [];
-        }, 750);
+        }, 500);
 }
+         addMove();
 }
-
+//Checking if game is over
   function gameOver() {
    if (matchedCards.length === cards.length) {
     setTimeout(function() {
           flippedCards = [];
            alert("Game over");
-        }, 750);
+        }, 500);
    }
   }
 
-  //start game 
-  startGame();
+  //add moves
+  const movesContainer = document.querySelector(".moves");
+  let moves = 0;
+  movesContainer.innerHTML = 0;
+  function addMove() {
+    moves++;
+    movesContainer.innerHTML = moves;
+    //set rating
+    rating();
+  }
 
 /*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+*star rating
+*/
+const starRating = document.querySelector(".stars");
+function rating() {
+    if(moves < 12) {
+        starRating.innerHTML = `<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>`;
+    } else if  ( moves < 18) { starRating.innerHTML = `<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li> `; 
+    } else  {
+        starRating.innerHTML = `<li><i class="fa fa-star"></i></li>`;
     }
+};
 
-    return array;
-}
+//restart button
+const restartBtn = document.querySelector(".restart");
+restartBtn.addEventListener("click", function(){
+    //CLear cards
+    cardTable.innerHTML = "";
+    // call to create more cards
+    startGame(shuffle(cards));
+    // Reset related variables
+    matchedCards = [];
+    moves = 0;
+    movesContainer.innerHTML = 0;
+    starRating.innerHTML = `<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>`;
+});
+  //start game 
+  startGame(shuffle(cards));
+   starRating.innerHTML = `<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>`;
+
+
+
 
 
 /*
