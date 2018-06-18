4dae942f3254b3dfd732fe82e531ec65c1ee8e3e
Brian W <39372876+BRWelk@users.noreply.github.com>
2018-06-17 19:28:22 -0500

    font size to vw for responsive design

--- fend-project-memory-game-BWELK/js/app.js
+++ fend-project-memory-game-BWELK/js/app.js
@@ -154,9 +154,9 @@ function addMove() {
 const starRating = document.querySelector(".stars");
 
 function rating() {
-  if (moves < 12) {
+  if (moves < 14) {
     starRating.innerHTML = `<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>`;
-  } else if (moves < 18) {
+  } else if (moves < 20) {
     starRating.innerHTML = `<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li> `;
   } else {
     starRating.innerHTML = `<li><i class="fa fa-star"></i></li>`;
@@ -183,6 +183,6 @@ restartBtn.addEventListener("click", function() {
 });
 //start game
 startGame(shuffle(cards));
-starRating.innerHTML = `<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>`;
+
 
 /* set up the event listener for a card. If a card is clicked:  - display the card's symbol (put this functionality in another function that you call from this one)  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)  - if the list already has another card, check to see if the two cards match    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one) */
